3ab26bcf44bf278d1dd1f79efc284c3162e50d37
John <j@soft.fish>
2024-02-28 05:00:39 -0600

    lexer: implement std::error:;Error for Error (whoops!)

--- libconlang/src/lexer.rs
+++ libconlang/src/lexer.rs
@@ -515,6 +515,7 @@ pub mod error {
             })*
         }
     }
+    impl std::error::Error for Error {}
     impl Display for Error {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             write!(f, "{}:{}: {}", self.line, self.col, self.reason)
