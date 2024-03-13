213a40514a9d33ea7c6a29e54b0db3b35a4cfd7a
John <j@soft.fish>
2024-03-12 19:32:11 -0500

    cl-structures: break `span` into its own file.

---
+++ cl-structures/src/span.rs
@@ -0,0 +1,38 @@
+//! - [struct@Span]: Stores the start and end [struct@Loc] of a notable AST node
+//! - [struct@Loc]: Stores the line/column of a notable AST node
+#![allow(non_snake_case)]
+
+/// Stores the start and end [locations](struct@Loc) within the token stream
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Span {
+    pub head: Loc,
+    pub tail: Loc,
+}
+pub fn Span(head: Loc, tail: Loc) -> Span {
+    Span { head, tail }
+}
+
+/// Stores a read-only (line, column) location in a token stream
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Loc {
+    line: u32,
+    col: u32,
+}
+pub fn Loc(line: u32, col: u32) -> Loc {
+    Loc { line, col }
+}
+impl Loc {
+    pub fn line(self) -> u32 {
+        self.line
+    }
+    pub fn col(self) -> u32 {
+        self.col
+    }
+}
+
+impl std::fmt::Display for Loc {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Loc { line, col } = self;
+        write!(f, "{line}:{col}:")
+    }
+}
