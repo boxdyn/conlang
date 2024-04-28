0e6829f83b9d4125ea65ac3e04a8bac80fcd912b
John <j@soft.fish>
2024-04-27 21:15:45 -0500

    cl-structures: Mention new structures in the top-module blurb

--- compiler/cl-structures/src/lib.rs
+++ compiler/cl-structures/src/lib.rs
@@ -1,6 +1,11 @@
 //! # Universally useful structures
 //! - [Span](struct@span::Span): Stores a start and end [Loc](struct@span::Loc)
 //! - [Loc](struct@span::Loc): Stores the index in a stream
+//! - [TypedInterner][ti] & [StringInterner][si]: Provies stable, unique allocations
+//! - [Stack](stack::Stack): Contiguous collections with constant capacity
+//! 
+//! [ti]: intern::typed_interner::TypedInterner
+//! [si]: intern::string_interner::StringInterner
 #![warn(clippy::all)]
 #![feature(inline_const, dropck_eyepatch, decl_macro, get_many_mut)]
 #![deny(unsafe_op_in_unsafe_fn)]
