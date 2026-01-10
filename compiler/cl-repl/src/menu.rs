13e972505d0f2d4a5a6cc332afd2045265ca21cb
John <j@soft.fish>
2026-01-10 02:23:06 -0500

    cl-repl/menu: Update to Repline 0.0.12

--- compiler/cl-repl/src/menu.rs
+++ compiler/cl-repl/src/menu.rs
@@ -5,7 +5,7 @@ use cl_ast::Stmt;
 use cl_interpret::convalue::ConValue;
 use cl_lexer::Lexer;
 use cl_parser::Parser;
-use repline::{Error as RlError, error::ReplResult, prebaked::*};
+use repline::{error::ReplResult, prebaked::*};
 
 pub fn clear() {
     print!("{}", ansi::CLEAR_ALL);
@@ -21,9 +21,9 @@ type ReplMode = (&'static str, &'static str, &'static str, ReplCallback);
 
 #[rustfmt::skip]
 const MODES: &[ReplMode] = &[
-    (ansi::CYAN,            " .>", "  >", mode_run),
-    (ansi::BRIGHT_BLUE,     " .>", "  >", mode_lex),
-    (ansi::BRIGHT_MAGENTA,  " .>", "  >", mode_fmt),
+    (ansi::CYAN,            " .> ", "  > ", mode_run),
+    (ansi::BRIGHT_BLUE,     " .> ", "  > ", mode_lex),
+    (ansi::BRIGHT_MAGENTA,  " .> ", "  > ", mode_fmt),
 ];
 
 const fn get_mode(mode: Mode) -> ReplMode {
@@ -36,55 +36,31 @@ const fn get_mode(mode: Mode) -> ReplMode {
 
 /// Presents a selection interface to the user
 pub fn main_menu(mode: Mode, ctx: &mut ctx::Context) -> ReplResult<()> {
-    let mut mode = get_mode(mode);
     banner();
 
-    let mut rl = repline::Repline::new(mode.0, mode.1, mode.2);
-    loop {
-        rl.set_prompt(mode.0, mode.1, mode.2);
-
-        let line = match rl.read() {
-            Err(RlError::CtrlC(_)) => return Ok(()),
-            Err(RlError::CtrlD(line)) => {
-                rl.deny();
-                line
-            }
-            Ok(line) => line,
-            Err(e) => Err(e)?,
-        };
-        print!("\x1b[G\x1b[J");
+    let mut mode = get_mode(mode);
+    read_and_mut(mode.0, mode.1, mode.2, |rl, line| {
         match line.trim() {
-            "" => continue,
-            "clear" => clear(),
-            "mode run" => mode = get_mode(Mode::Run),
-            "mode lex" => mode = get_mode(Mode::Lex),
-            "mode fmt" => mode = get_mode(Mode::Fmt),
-            "quit" => return Ok(()),
+            "" => return Ok(Response::Continue),
             "help" => println!(
                 "Valid commands
     help     : Print this list
     clear    : Clear the screen
-    quit     : Exit the program
+    exit     : Exit the program
     mode lex : Lex the input
     mode fmt : Format the input
     mode run : Evaluate some expressions"
             ),
-            _ => match mode.3(ctx, &line) {
-                Ok(Response::Continue) => continue,
-                Ok(Response::Break) => return Ok(()),
-                Ok(Response::Deny) => {}
-                Ok(Response::Accept) => {
-                    rl.accept();
-                    continue;
-                }
-                Err(e) => {
-                    rl.print_inline(format_args!("\t\x1b[31m{e}\x1b[0m"))?;
-                    continue;
-                }
-            },
+            "clear" => clear(),
+            "exit" => return Ok(Response::Break),
+            "mode lex" => mode = get_mode(Mode::Lex),
+            "mode fmt" => mode = get_mode(Mode::Fmt),
+            "mode run" => mode = get_mode(Mode::Run),
+            _ => return mode.3(ctx, line),
         }
-        rl.deny();
-    }
+        rl.set_prompt(mode.0, mode.1, mode.2);
+        Ok(Response::Accept)
+    })
 }
 
 pub fn mode_run(ctx: &mut ctx::Context, line: &str) -> Result<Response, Box<dyn Error>> {
