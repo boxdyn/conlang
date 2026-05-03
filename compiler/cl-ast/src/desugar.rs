6bbc87f7c85cfb08da8d6564f8505e08713289b0
John <j@soft.fish>
2026-05-03 12:31:52 -0400

    ast/desugar: remove unsupported passes

--- compiler/cl-ast/src/desugar.rs
+++ compiler/cl-ast/src/desugar.rs
@@ -1,12 +1,5 @@
 //! Desugaring passes for Conlang
 
+pub mod squash_groups;
 pub mod type_bubbler;
-// pub mod constant_folder;
-// pub mod path_absoluter;
-// pub mod squash_groups;
 pub mod while_else;
-
-// pub use constant_folder::ConstantFolder;
-// pub use path_absoluter::NormalizePaths;
-// pub use squash_groups::SquashGroups;
-// pub use while_else::WhileElseDesugar;
