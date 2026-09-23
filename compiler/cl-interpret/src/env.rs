d204fd43baa1a59bc08d1b9e95090e791df5aaa7
John <j@soft.fish>
2026-09-23 07:29:51 -0400

    cl-interpret/env: dump stack downwards

--- compiler/cl-interpret/src/env.rs
+++ compiler/cl-interpret/src/env.rs
@@ -66,7 +66,7 @@ pub struct Environment {
 impl Display for Environment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use std::fmt::Write as _;
-        for EnvFrame { name, binds, .. } in self.frames.iter().rev() {
+        for EnvFrame { name, binds, .. } in self.frames.iter() {
             writeln!(
                 f,
                 "--- {}[{}] ---",
